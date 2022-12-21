428bc6efa6e4d4fa4ddfed5695562091829d90b4
Mario Palic <mario.palic@deptagency.com>
2022-12-21 14:00:53 +0100

    chore: Report file - extend failure message

--- junit.js
+++ junit.js
@@ -6,6 +6,13 @@ var replacements = {
   '"': '&quot;',
 };
 
+const counterMetrics = [ "data_received", "data_sent", "iterations", "http_reqs"];
+const gaugeMetrics = ["vus", "vus_max"];
+const rateMetrics = ["http_req_failed", "checks"];
+const trendMetrics = ["http_req_sending", "iteration_duration", "http_req_duration", "http_req_connecting", 
+                      "http_req_waiting", "http_req_blocked", "http_req_waiting", "http_req_receiving",
+                      "http_req_tls_handshaking", ];
+
 function escapeHTML(str) {
   // TODO: something more robust?
   return str.replace(/[&<>'"]/g, function (char) {
@@ -32,12 +39,24 @@ function generateJUnitXML(data, options) {
         );
       } else {
         failures++;
+        var failureMessage = ""
+        if (counterMetrics.includes(metricName)) {
+          failureMessage = '"><failure message="failed, count: ' + metric.values.count + '"/></testcase>';
+        } 
+        else if (gaugeMetrics.includes(metricName)){
+          failureMessage = '"><failure message="failed, value: ' + metric.values.value + '"/></testcase>';
+        }
+        else if (rateMetrics.includes(metricName)) {
+          failureMessage = '"><failure message="failed, number of fails: ' + metric.values.fails + '"/></testcase>';
+        }
+        else if (trendMetrics.includes(metricName)) {
+          failureMessage = '"><failure message="failed, number of fails: ' + metric.values.fails + '"/></testcase>';
+        }
         cases.push(
           '<testcase name="' +
             escapeHTML(metricName) +
             ' - ' +
-            escapeHTML(thresholdName) +
-            '"><failure message="failed" /></testcase>',
+            escapeHTML(thresholdName) + failureMessage
         );
       }
     });
