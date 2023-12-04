59b2c29d925139ada13f8dd656c4f8ab26836d95
Oleg Bespalov <oleg.bespalov@grafana.com>
2023-12-04 13:26:35 +0100

    Adds package.json, dev-dependencies, moves sources
    Co-authored-by: Mihail Stoykov <312246+mstoykov@users.noreply.github.com>

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// this is the entry point for the library
+// it exports all the functions that are available to the user
+// it uses also for the bundling and minifying
+// (see package.json's "build" script)
+export { jUnit } from './junit.js';
+export { humanizeValue, textSummary } from './text.js';
